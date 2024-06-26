17c09f536930f5a3ed7ecee40faf0e2c1a04aa02
Taizo <taizo.nakase@gmail.com>
2024-06-26 16:56:00 -0300

    Python para todos
    Stopped at page 147

--- python-para-todos/exercicios/10.11-03.py
+++ python-para-todos/exercicios/10.11-03.py
@@ -19,44 +19,39 @@ try:
     arquivo = open(_arquivo, 'r')
 
 except:
-    print('Arquiv não encontrado.')
+    print('Arquivo não encontrado.')
     exit()
 
 
 # Lê as linhas, retira o indesejado e coloca tudo em uma grande string
 for linhas in arquivo:
-    linhas = linhas.rstrip()
+    linhas = linhas.strip()
     linhas = linhas.translate(linhas.maketrans('', '', string.punctuation))
-    linhas = linhas.translate(linhas.maketrans('', '', ))
+    linhas = linhas.replace(' ', '')
     linhas = linhas.lower()
 
     # Condição para retirar linhas vazias
     if (len(linhas) == 0): continue
-
+    
     # Iteração para contar apenas letras
-    numeros = (0, 1, 2, 3, 4, 5, 6, 7, 8, 9)   
+    numeros = ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')   
     for letra in linhas:
         if letra not in numeros:
             if letra not in cont:
                 cont[letra] = 1
             else:
                 cont[letra] += 1
-
-    for a in cont:
-        print(cont)
-
-
-    '''
-    # Coloca o dicionário em uma lista para poder contar
-    for x, y in cont.items():
-        cont_aj.append((y, x))
     
 
-    # Ordena a nova lista
-    cont_aj.sort()
+# Coloca o dicionário em uma lista para poder contar
+for x, y in cont.items():
+    cont_aj.append((y, x))
 
-    # Mostra a lista na tela
-    for x, y in cont_aj:
-        print(y, x)
+# Ordena a nova lista
+cont_aj.sort(reverse=True)
 
-    '''
+# Mostra a lista na tela
+for x, y in cont_aj:
+    print(y, x)
+
+    
