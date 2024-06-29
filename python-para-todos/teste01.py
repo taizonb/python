1eebf0315a663822d012a4307ef60f2a05db06e4
Taizo <taizo.nakase@gmail.com>
2024-06-29 17:41:50 -0300

    Python para todos
    Stopped at page 158.

--- python-para-todos/teste01.py
+++ python-para-todos/teste01.py
@@ -56,7 +56,7 @@ for key, val in lst[:10]:
     print(key, val)
 '''
 
-import socket
+'''import socket
 
 mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 mysock.connect(('data.pr4e.org', 80))
@@ -69,4 +69,65 @@ while True:
         break
     print(data.decode(), end='')
 
-mysock.close()    
+mysock.close()    '''
+
+
+'''import socket
+import time
+HOST = 'data.pr4e.org'
+PORT = 80
+mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mysock.connect((HOST, PORT))
+mysock.sendall(b'GET http://data.pr4e.org/cover3.jpg HTTP/1.0\r\n\r\n')
+count = 0
+picture = b""
+while True:
+    data = mysock.recv(5120)
+    if len(data) < 1: break
+    #time.sleep(0.25)
+    count = count + len(data)
+    print(len(data), count)
+    picture = picture + data
+mysock.close()
+# Look for the end of the header (2 CRLF)
+pos = picture.find(b"\r\n\r\n")
+print('Header length', pos)
+print(picture[:pos].decode())
+# Skip past the header and save the picture data
+picture = picture[pos+4:]
+fhand = open("stuff.jpg", "wb")
+fhand.write(picture)
+fhand.close()'''
+
+
+'''import urllib.request, urllib.parse, urllib.error
+
+fhand = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
+
+counts = dict()
+for line in fhand:
+    words = line.decode().split()
+
+    for word in words:
+        counts[word] = counts.get(word, 0) + 1
+
+
+print(counts)'''
+
+
+# Search for link values within URL input
+import urllib.request, urllib.parse, urllib.error
+import re
+import ssl
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input('Enter - ')
+html = urllib.request.urlopen(url).read()
+links = re.findall(b'href="(http[s]?://.*?)"', html)
+for link in links:
+    print(link.decode())
+# Code: http://www.py4e.com/code3/urlregex.py
