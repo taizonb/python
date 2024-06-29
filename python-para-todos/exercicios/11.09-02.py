ebb198c84e18c21cfe22893e9a6b96141b7c3080
Taizo <taizo.nakase@gmail.com>
2024-06-29 12:28:19 -0300

    Python para todos
    Stopped at page 149.

--- python-para-todos/exercicios/11.09-02.py
+++ python-para-todos/exercicios/11.09-02.py
@@ -14,15 +14,41 @@ Arquivo de entrada: mbox-short.txt
 
 # Solução 01:
 
-_arquivo = input('Arquivo de entrada: ')
+import re
 
+
+count = 0
+sum = 0
+
+# Abre o arquivo desejado:
 teste = 0
+_arquivo = input('Arquivo de entrada: ')
 while (teste == 0):
     try:
         arquivo = open(_arquivo, 'r')
         teste = 1
     except:
-        print('Arquivo não encontrado.')
+        _arquivo = input('Arquivo não encontrado. Digite novamente: ')
         teste = 0
 
-print('agora foi!')
+
+# Procura o termo na linha:
+for linhas in arquivo:
+    linhas = linhas.strip()
+    termo = re.findall('^New Revision: ([0-9.]+)', linhas)
+    
+    if (len(termo) > 0):
+        for val in termo:
+            count += 1
+            sum += float(val)
+            print(f'Nova Revisão: {val}')
+
+
+# Verifica se foram encontrados
+if (count == 0):
+    print('Não foram encontrados valores')
+    exit
+# Calcula a média dos valores e mostra na tela:
+else:
+    med = sum/count
+    print(f'Média: {med}')
