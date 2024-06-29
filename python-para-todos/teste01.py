dc76b2216bddcef1bb61fea738cfaa9998addc2f
Taizo <taizo.nakase@gmail.com>
2024-06-29 13:55:43 -0300

    Python para todos.

--- python-para-todos/teste01.py
+++ python-para-todos/teste01.py
@@ -27,6 +27,8 @@ for line in fhand:
         count = count + 1
 print('There were', count, 'subject lines in', fname)'''
 
+
+'''
 import string
 
 fhand = open('romeo.txt')
@@ -52,3 +54,19 @@ lst.sort(reverse=True)
 
 for key, val in lst[:10]:
     print(key, val)
+'''
+
+import socket
+
+mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mysock.connect(('data.pr4e.org', 80))
+cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\r\n\r\n'.encode()
+mysock.send(cmd)
+
+while True:
+    data = mysock.recv(512)
+    if len(data) < 1:
+        break
+    print(data.decode(), end='')
+
+mysock.close()    
