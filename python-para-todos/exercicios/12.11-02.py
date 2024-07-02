554032c5ef1f75ca5972ec07c71e00e007903e13
Taizo <taizo.nakase@gmail.com>
2024-07-02 16:01:12 -0300

    Python para todos

--- python-para-todos/exercicios/12.11-02.py
+++ python-para-todos/exercicios/12.11-02.py
@@ -1,5 +1,7 @@
 '''
 Exercício 2: Altere seu programa de soquete para que ele conte o número de caracteres que recebeu e pare de mostrar qualquer texto depois que mostrar 3000 caracteres. O programa deve recuperar o documento inteiro e contar o número total de caracteres e mostrar o resultado da contagem no final do documento.
+
+site teste: http://data.pr4e.org/romeo.txt
 '''
 
 
@@ -18,16 +20,15 @@ try:
     mysock.connect((host_url[2], 80))   # Conecta ao servidor
     cmd = f'GET {_url} HTTP/1.0\r\n\r\n'.encode()   # Ajusta a url no padrão
     mysock.send(cmd)   # Envia a URL ajustadapara para o servidor
+    data = mysock.recv(510)   # Recebe os primeiros "X" caracteres
+    print(data.decode(),end='')
 
-    data2 = mysock.recv(510)   # Recebe os primeiros "X" caracteres
+    count += len(data)   # Conta a qtd de caracteres total
 
-    while True:
-        data = mysock.recv(3000)   # Loop que recebe o restante dos caracteres
+    while (len(data) > 1):
+        data = mysock.recv(510)   # Loop que recebe o restante dos caracteres
         count += len(data)   # Conta a qtd de caracteres total
-        if len(data) < 1:   # Sai do loop quando acabam os caracteres
-            break
         mysock.close()
-        
 
 # Caso de errado, sai do loop e fecha o programa
 except:
@@ -36,7 +37,4 @@ except:
 
 # Mostra as informações na tela e conta o total de caracteres
 if (count != 0):
-    print(data2.decode(),end='')
-    total = len(data) + len(data2)
-
-    print(f'\nQuantidade de caracteres: {total}')
+    print(f'\nQuantidade de caracteres: {count}')
--- python-para-todos/exercicios/12.11-03.py
+++ python-para-todos/exercicios/12.11-03.py
@@ -1,7 +1,45 @@
 '''
 Exercício 3: Use urllib para replicar o exercício anterior (1) recuperando um documento de uma URL, (2) mostrando até 3000 caracteres e (3) contando o total destes no documento. Não se preocupe sobre os cabeçalhos para esse exercício, apenas mostre os 3000 primeiros caracteres do contepudo do documento.
+
+site teste: http://data.pr4e.org/romeo.txt
 '''
 
 
 # Solução 01:
 
+import urllib.request, urllib.parse, urllib.error
+import string
+
+
+palavras_limite = 115
+palavras_qtd = 0
+palavras_total = 0
+
+
+# Pega o site digitado
+_site = input('Digite o site que deseja: ')
+#site = _site.split('/')  # pega somente o servidor
+#site = site[2]
+
+
+try:
+    site = urllib.request.urlopen(_site)
+    
+    for linhas in site:
+        linhas = linhas.decode().strip()
+        palavras_qtd = len(linhas)
+        palavras_total += len(linhas)
+
+        if (palavras_total <= palavras_limite):
+            print(linhas)
+        else:
+            print(linhas[:palavras_total - (palavras_total - palavras_limite)])
+
+        print(palavras_qtd)       
+        print(palavras_total)   
+except:
+    print('Site digitado incorreto. ')
+
+
+if (palavras_qtd > 1):
+    print(f'\nQuantidade de caracteres totais: {palavras_total}')
