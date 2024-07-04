d3d84fef8e5ef0534e565de0a3019c32ba40afc3
Taizo <taizo.nakase@gmail.com>
2024-07-04 15:29:14 -0300

    Python para todos

--- python-para-todos/exercicios/12.11-01.py
+++ python-para-todos/exercicios/12.11-01.py
@@ -1,5 +1,7 @@
 '''
 Exercício 1: Altere o programa de soquete socket1.py pR pedir ao usuário a URL para que então possa ler qualquer página da web.Você pode usar split('/') para quebrar a URL em suas componentes para que então possa extrair o nome do hospedeiro para que o soquete connect chame. Adicione tratamento de erro usando try e except para lidar com a condição do usuário digitar uma URL formatada incorretamente ou uma não existente
+
+site teste: http://data.pr4e.org/romeo.txt
 '''
 
 
@@ -7,24 +9,19 @@ Exercício 1: Altere o programa de soquete socket1.py pR pedir ao usuário a URL
 
 import socket
 
+# Importando a URL
 _url = input('Digite uma URL: ')
+mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host_url = _url.split('/')   # Quebra a URL para o dominio
+mysock.connect((host_url[2], 80))
+cmd = f'GET {_url} HTTP/1.0\r\n\r\n'.encode()
+mysock.send(cmd)
 
-try:
-    mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    host_url = _url.split('/')
+while True:
+    data = mysock.recv(512)
+    if len(data) < 1:
+        break
+    print(data.decode(),end='')
 
-    print(host_url[2])
-
-    mysock.connect((host_url[2], 80))
-    cmd = f'GET {_url} HTTP/1.0\r\n\r\n'.encode()
-    mysock.send(cmd)
-    while True:
-        data = mysock.recv(512)
-        if len(data) < 1:
-            break
-        print(data.decode(),end='')
-        mysock.close()
-
-except:
-    print('URL digitada incorreta.')
+mysock.close()
--- python-para-todos/exercicios/12.11-05.py
+++ python-para-todos/exercicios/12.11-05.py
@@ -1,7 +1,25 @@
 '''
 Exercício 5: (Avançado) Altere o programa de soquete para que ele apenas mostre uma informação depois dos cabeçalhos e uma linha em branco ser recebida. Lembre que recv recebe caracteres (newlines e etc), não linhas.
+
+site teste: http://data.pr4e.org/romeo.txt
 '''
 
 
 # Solução 01:
 
+import socket
+
+# Importando a URL
+_url = input('Digite uma URL: ')
+mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host_url = _url.split('/')   # Quebra a URL para o dominio
+mysock.connect((host_url[2], 80))
+cmd = f'GET {_url} HTTP/1.0\r\n\r\n'.encode()
+mysock.send(cmd)
+data = mysock.recv(512)
+
+# Procura o final do cabeçalho
+message = data.decode()
+header_finish = message.find('\n')
+
+print(message)
