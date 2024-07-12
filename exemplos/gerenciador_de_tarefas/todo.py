747b503dffdef3e90ba8c54d8922ce9e151a8fd2
Taizo <taizo.nakase@gmail.com>
2024-07-12 11:51:19 -0300

    todo.py
    finished the app.

--- exemplos/gerenciador_de_tarefas/todo.py
+++ exemplos/gerenciador_de_tarefas/todo.py
@@ -12,7 +12,9 @@ class ToDo:
         self.page.window_always_on_top = True
         self.page.title = 'ToDo App'
         self.task = ''
+        self.view = 'all'
         self.db_execute('CREATE TABLE IF NOT EXISTS tasks(name, status)')
+        self.results = self.db_execute('SELECT * FROM tasks')
         self.main_page()
 
     def db_execute(self, query, params = []):
@@ -22,12 +24,31 @@ class ToDo:
             con.commit()
             return cur.fetchall()
 
+    def checked(self, e):
+        is_checked = e.control.value
+        label = e.control.label
+
+        if is_checked:
+            self.db_execute('UPDATE tasks SET status = "complete" WHERE name = ?', params=[label])
+        else:
+            self.db_execute('UPDATE tasks SET status = "incomplete" WHERE name = ?', params=[label])
+
+        if self.view == 'all':
+            self.results = self.db_execute('SELECT * FROM tasks')
+        else:
+            self.results = self.db_execute('SELECT * FROM tasks WHERE status = ?', params=[self.view])
+        
+        self.update_task_list()
+
     def tasks_container(self):
         return ft.Container(
             height = self.page.height*0.8,
             content = ft.Column(
                 controls = [
-                    ft.Checkbox(label='Tarefa 1', value = True)
+                    ft.Checkbox(label=res[0],
+                                on_change = self.checked,
+                                value = True if res[1] == 'complete' else False)
+                    for res in self.results if res
                 ]
             )
         )
@@ -39,8 +60,30 @@ class ToDo:
         name = self.task
         status = 'incomplete'
 
-        #parei aqui -> 26:27
+        if name:
+            self.db_execute(query='INSERT INTO tasks VALUES(?,?)', params=[name, status])
+            input_task.value = ''
+            self.results = self.db_execute('SELECT * FROM tasks')
+            self.update_task_list()
 
+    def update_task_list(self):
+        tasks = self.tasks_container()
+        self.page.controls.pop()
+        self.page.add(tasks)
+        self.page.update()
+
+    def tabs_changed(self, e):
+        if e.control.selected_index == 0:
+            self.results = self.db_execute('SELECT * FROM tasks')
+            self.view == 'all'
+        elif e.control.selected_index == 1:
+            self.results = self.db_execute('SELECT * FROM tasks WHERE status = "incomplete"')
+            self.view == 'incomplete'
+        elif e.control.selected_index == 2:
+            self.results = self.db_execute('SELECT * FROM tasks WHERE status = "complete"')
+            self.view == 'complete'
+
+        self.update_task_list()
 
     def main_page(self):
         input_task = ft.TextField(hint_text = 'Digite uma tarefa', expand=True, on_change=self.set_value)
@@ -57,6 +100,7 @@ class ToDo:
 
         tabs = ft.Tabs(
             selected_index=0,
+            on_change=self.tabs_changed,
             tabs=[
                 ft.Tab(text='Todos'),
                 ft.Tab(text='Em Andamento'),
